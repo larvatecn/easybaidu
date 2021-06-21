b56cbfec664d24a58909b83178299b461674b6a1
Larva Tech <support@larvacent.com>
2021-06-22 06:20:37 +0800

    add Config

---
+++ src/Kernel/Config.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace EasyBaidu\Kernel;
+
+use EasyBaidu\Kernel\Support\Collection;
+
+/**
+ * Class Config.
+ *
+ * @author overtrue <i@overtrue.me>
+ */
+class Config extends Collection
+{
+}
