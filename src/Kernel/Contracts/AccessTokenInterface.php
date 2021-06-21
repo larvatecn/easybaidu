9dab2fa07a37fd39897dac84afdacca058cb621c
Larva Tech <support@larvacent.com>
2021-06-22 06:44:51 +0800

    cc

---
+++ src/Kernel/Contracts/AccessTokenInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace EasyBaidu\Kernel\Contracts;
+
+use Psr\Http\Message\RequestInterface;
+
+/**
+ * Interface AuthorizerAccessToken.
+ *
+ * @author overtrue <i@overtrue.me>
+ */
+interface AccessTokenInterface
+{
+    /**
+     * @return array
+     */
+    public function getToken(): array;
+
+    /**
+     * @return \EasyBaidu\Kernel\Contracts\AccessTokenInterface
+     */
+    public function refresh(): self;
+
+    /**
+     * @param \Psr\Http\Message\RequestInterface $request
+     * @param array                              $requestOptions
+     *
+     * @return \Psr\Http\Message\RequestInterface
+     */
+    public function applyToRequest(RequestInterface $request, array $requestOptions = []): RequestInterface;
+}
